db6b84ce2be55676b7de8750e95ebf3dcd064724
utzcoz <utzcoz@outlook.com>
2019-10-20 22:45:13 +0800

    Simply the UI of DevicesActivity
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- app/src/main/java/com/github/dlna/DevicesActivity.java
+++ app/src/main/java/com/github/dlna/DevicesActivity.java
@@ -1,7 +1,5 @@
 package com.github.dlna;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -10,25 +8,15 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.dlna.application.BaseApplication;
 import com.github.dlna.dmp.DeviceItem;
 import com.github.dlna.dmr.ZxtMediaRenderer;
 import com.github.dlna.dms.MediaServer;
 import com.github.dlna.util.FixedAndroidHandler;
-import com.github.dlna.util.Utils;
 
 import org.fourthline.cling.android.AndroidUpnpService;
 import org.fourthline.cling.android.AndroidUpnpServiceImpl;
@@ -39,24 +27,17 @@ import org.fourthline.cling.registry.Registry;
 import org.seamless.util.logging.LoggingUtil;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DevicesActivity extends Activity {
+public class DevicesActivity extends AppCompatActivity {
+    private static final Logger log = Logger.getLogger(DevicesActivity.class.getName());
 
-    private static final Logger log = Logger.getLogger(DevicesActivity.class
-            .getName());
+    private final static String TAG = "DevicesActivity";
 
-    private final static String LOGTAG = "DevicesActivity";
-
-    private ListView mDmrLv;
-
-    private ArrayList<DeviceItem> mDmrList = new ArrayList<DeviceItem>();
+    private ArrayList<DeviceItem> mDmrList = new ArrayList<>();
 
     private long exitTime = 0;
-
-    private DevAdapter mDmrDevAdapter;
 
     private AndroidUpnpService upnpService;
 
@@ -67,20 +48,17 @@ public class DevicesActivity extends Activity {
     private ServiceConnection serviceConnection = new ServiceConnection() {
 
         public void onServiceConnected(ComponentName className, IBinder service) {
-
             mDmrList.clear();
 
             upnpService = (AndroidUpnpService) service;
             BaseApplication.upnpService = upnpService;
 
-            Log.v(LOGTAG, "Connected to UPnP Service");
+            Log.v(TAG, "Connected to UPnP Service");
 
-            if (mediaServer == null
-                    && Settings.getDmsOn()) {
+            if (mediaServer == null && Settings.getDmsOn()) {
                 try {
                     mediaServer = new MediaServer(DevicesActivity.this);
-                    upnpService.getRegistry()
-                            .addDevice(mediaServer.getDevice());
+                    upnpService.getRegistry().addDevice(mediaServer.getDevice());
                 } catch (Exception ex) {
                     // TODO: handle exception
                     log.log(Level.SEVERE, "Creating demo device failed", ex);
@@ -99,15 +77,12 @@ public class DevicesActivity extends Activity {
                         mediaRenderer.getDevice()));
             }
 
-            // xgf
-
             // Getting ready for future device advertisements
             upnpService.getRegistry().addListener(deviceListRegistryListener);
             // Refresh device list
             upnpService.getControlPoint().search();
 
-            if (null != mDmrList && mDmrList.size() > 0
-                    && null == BaseApplication.dmrDeviceItem) {
+            if (null != mDmrList && mDmrList.size() > 0 && null == BaseApplication.dmrDeviceItem) {
                 BaseApplication.dmrDeviceItem = mDmrList.get(0);
             }
         }
@@ -131,79 +106,26 @@ public class DevicesActivity extends Activity {
 
         deviceListRegistryListener = new DeviceListRegistryListener();
 
-        getApplicationContext().bindService(
-                new Intent(this, AndroidUpnpServiceImpl.class),
-                serviceConnection, Context.BIND_AUTO_CREATE);
+        getApplicationContext()
+                .bindService(
+                        new Intent(this, AndroidUpnpServiceImpl.class),
+                        serviceConnection, Context.BIND_AUTO_CREATE
+                );
     }
 
     private void init() {
-        mDmrLv = findViewById(R.id.renderer_list);
-
         if (null != mDmrList && mDmrList.size() > 0) {
             BaseApplication.dmrDeviceItem = mDmrList.get(0);
         }
-
-        mDmrDevAdapter = new DevAdapter(DevicesActivity.this, 0, mDmrList);
-        mDmrLv.setAdapter(mDmrDevAdapter);
-        mDmrLv.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-                                    long arg3) {
-
-                if (null != mDmrList && mDmrList.size() > 0) {
-
-                    BaseApplication.isLocalDmr = null != mDmrList.get(arg2).getDevice()
-                            && null != BaseApplication.deviceItem
-                            && null != mDmrList.get(arg2).getDevice()
-                            .getDetails().getModelDetails()
-                            && Utils.DMR_NAME.equals(mDmrList.get(arg2)
-                            .getDevice().getDetails().getModelDetails()
-                            .getModelName())
-                            && Utils.getDevName(
-                            mDmrList.get(arg2).getDevice().getDetails()
-                                    .getFriendlyName()).equals(
-                            Utils.getDevName(BaseApplication.deviceItem
-                                    .getDevice().getDetails()
-                                    .getFriendlyName()));
-                    BaseApplication.dmrDeviceItem = mDmrList.get(arg2);
-                    mDmrDevAdapter.notifyDataSetChanged();
-                }
-            }
-        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (upnpService != null) {
-            upnpService.getRegistry()
-                    .removeListener(deviceListRegistryListener);
+            upnpService.getRegistry().removeListener(deviceListRegistryListener);
         }
         getApplicationContext().unbindService(serviceConnection);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, 0, 0, R.string.search_lan).setIcon(
-                android.R.drawable.ic_menu_search);
-        menu.add(0, 1, 0, R.string.menu_exit).setIcon(
-                android.R.drawable.ic_menu_close_clear_cancel);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case 0:
-                searchNetwork();
-                break;
-            case 1: {
-                finish();
-                System.exit(0);
-                break;
-            }
-        }
-        return false;
     }
 
     @Override
@@ -224,8 +146,9 @@ public class DevicesActivity extends Activity {
     }
 
     protected void searchNetwork() {
-        if (upnpService == null)
+        if (upnpService == null) {
             return;
+        }
         Toast.makeText(this, R.string.searching_lan, Toast.LENGTH_SHORT).show();
         upnpService.getRegistry().removeAllRemoteDevices();
         upnpService.getControlPoint().search();
@@ -242,27 +165,27 @@ public class DevicesActivity extends Activity {
 
         @Override
         public void remoteDeviceDiscoveryFailed(Registry registry,
-                                                final RemoteDevice device, final Exception ex) {
+                                                final RemoteDevice device,
+                                                final Exception ex) {
         }
 
-        /*
+        /**
          * End of optimization, you can remove the whole block if your Android
          * handset is fast (>= 600 Mhz)
          */
-
-        @SuppressLint("LongLogTag")
         @Override
         public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
-            Log.e("DeviceListRegistryListener",
-                    "remoteDeviceAdded:" + device.toString()
-                            + device.getType().getType());
+            Log.e(TAG, "remoteDeviceAdded:" + device.toString() + device.getType().getType());
 
             if (device.getType().getNamespace().equals("schemas-upnp-org")
                     && device.getType().getType().equals("MediaRenderer")) {
-                final DeviceItem dmrDisplay = new DeviceItem(device, device
-                        .getDetails().getFriendlyName(),
-                        device.getDisplayString(), "(REMOTE) "
-                        + device.getType().getDisplayString());
+                final DeviceItem dmrDisplay =
+                        new DeviceItem(
+                                device,
+                                device.getDetails().getFriendlyName(),
+                                device.getDisplayString(),
+                                "(REMOTE) " + device.getType().getDisplayString()
+                        );
                 dmrAdded(dmrDisplay);
             }
         }
@@ -271,28 +194,25 @@ public class DevicesActivity extends Activity {
         public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
             if (device.getType().getNamespace().equals("schemas-upnp-org")
                     && device.getType().getType().equals("MediaRenderer")) {
-                final DeviceItem dmrDisplay = new DeviceItem(device, device
-                        .getDetails().getFriendlyName(),
-                        device.getDisplayString(), "(REMOTE) "
-                        + device.getType().getDisplayString());
+                final DeviceItem dmrDisplay =
+                        new DeviceItem(
+                                device,
+                                device.getDetails().getFriendlyName(),
+                                device.getDisplayString(),
+                                "(REMOTE) " + device.getType().getDisplayString()
+                        );
                 dmrRemoved(dmrDisplay);
             }
         }
 
-        @SuppressLint("LongLogTag")
         @Override
         public void localDeviceAdded(Registry registry, LocalDevice device) {
-            Log.e("DeviceListRegistryListener",
-                    "localDeviceAdded:" + device.toString()
-                            + device.getType().getType());
+            Log.e(TAG, "localDeviceAdded:" + device.toString() + device.getType().getType());
         }
 
-        @SuppressLint("LongLogTag")
         @Override
         public void localDeviceRemoved(Registry registry, LocalDevice device) {
-            Log.e("DeviceListRegistryListener",
-                    "localDeviceRemoved:" + device.toString()
-                            + device.getType().getType());
+            Log.e(TAG, "localDeviceRemoved:" + device.toString() + device.getType().getType());
         }
 
         void dmrAdded(final DeviceItem di) {
@@ -300,7 +220,6 @@ public class DevicesActivity extends Activity {
                 public void run() {
                     if (!mDmrList.contains(di)) {
                         mDmrList.add(di);
-                        mDmrDevAdapter.notifyDataSetChanged();
                     }
                 }
             });
@@ -310,69 +229,8 @@ public class DevicesActivity extends Activity {
             runOnUiThread(new Runnable() {
                 public void run() {
                     mDmrList.remove(di);
-                    mDmrDevAdapter.notifyDataSetChanged();
                 }
             });
         }
-    }
-
-    class DevAdapter extends ArrayAdapter<DeviceItem> {
-
-        private LayoutInflater mInflater;
-
-        private List<DeviceItem> deviceItems;
-
-        DevAdapter(Context context, int textViewResourceId,
-                   List<DeviceItem> objects) {
-            super(context, textViewResourceId, objects);
-            this.mInflater = ((LayoutInflater) context
-                    .getSystemService("layout_inflater"));
-            this.deviceItems = objects;
-        }
-
-        public int getCount() {
-            return this.deviceItems.size();
-        }
-
-        public DeviceItem getItem(int paramInt) {
-            return this.deviceItems.get(paramInt);
-        }
-
-        public long getItemId(int paramInt) {
-            return paramInt;
-        }
-
-        public View getView(int position, View view, ViewGroup viewGroup) {
-
-            DevHolder holder;
-            if (view == null) {
-                view = this.mInflater.inflate(R.layout.dmr_item, null);
-                holder = new DevHolder();
-                holder.filename = view.findViewById(R.id.dmr_name_tv);
-                holder.checkBox = view.findViewById(R.id.dmr_cb);
-                view.setTag(holder);
-            } else {
-                holder = (DevHolder) view.getTag();
-            }
-
-            DeviceItem item = this.deviceItems.get(position);
-            holder.filename.setText(item.toString());
-            if (null != BaseApplication.deviceItem
-                    && BaseApplication.deviceItem.equals(item)) {
-                holder.checkBox.setChecked(true);
-            } else if (null != BaseApplication.dmrDeviceItem
-                    && BaseApplication.dmrDeviceItem.equals(item)) {
-                holder.checkBox.setChecked(true);
-            } else {
-                holder.checkBox.setChecked(false);
-            }
-            return view;
-        }
-
-        final class DevHolder {
-            TextView filename;
-            CheckBox checkBox;
-        }
-
     }
 }
