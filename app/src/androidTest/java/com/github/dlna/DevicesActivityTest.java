2cd69003fddd780f73d650fbf70f4bd0f9807a1e
utzcoz <utzcoz@outlook.com>
2020-03-21 00:09:02 +0800

    Move upnp search test to a single test file
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- app/src/androidTest/java/com/github/dlna/DevicesActivityTest.java
+++ app/src/androidTest/java/com/github/dlna/DevicesActivityTest.java
@@ -3,11 +3,6 @@ package com.github.dlna;
 import androidx.lifecycle.Lifecycle;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
-import org.fourthline.cling.model.meta.DeviceDetails;
-import org.fourthline.cling.model.meta.ModelDetails;
-import org.fourthline.cling.model.meta.RemoteDevice;
-import org.fourthline.cling.model.types.DLNADoc;
-import org.fourthline.cling.model.types.UDADeviceType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,30 +23,6 @@ public class DevicesActivityTest extends TestBase {
         DevicesActivity activity = getActivity();
         assertNotNull(activity.getUpnpService());
         assertTrue(activity.hasConnected());
-    }
-
-    @Test
-    public void testTestUpnpServiceSearchDeviceSucceed() {
-        RemoteDevice remoteDevice = TestHelper.searchRemoteDevice(upnpService);
-        assertNotNull(remoteDevice);
-        assertEquals(Utils.uniqueSystemIdentifier(), remoteDevice.getIdentity().getUdn());
-        assertEquals(UDADeviceType.DEFAULT_NAMESPACE, remoteDevice.getType().getNamespace());
-        assertEquals("MediaRenderer", remoteDevice.getType().getType());
-        assertEquals(1, remoteDevice.getType().getVersion());
-        DeviceDetails deviceDetails = remoteDevice.getDetails();
-        assertEquals(
-                Utils.getRenderName() + " (" + android.os.Build.MODEL + ")",
-                deviceDetails.getFriendlyName()
-        );
-        assertEquals(Utils.MANUFACTURER, deviceDetails.getManufacturerDetails().getManufacturer());
-        ModelDetails modelDetails = deviceDetails.getModelDetails();
-        assertEquals(Utils.DMR_NAME, modelDetails.getModelName());
-        assertEquals(Utils.DMR_DESC, modelDetails.getModelDescription());
-        assertEquals("1", modelDetails.getModelNumber());
-        assertEquals(Utils.DMR_MODEL_URL, modelDetails.getModelURI().toString());
-        DLNADoc doc = deviceDetails.getDlnaDocs()[0];
-        assertEquals("DMR", doc.getDevClass());
-        assertEquals(DLNADoc.Version.V1_5.toString(), doc.getVersion());
     }
 
     private DevicesActivity getActivity() {
