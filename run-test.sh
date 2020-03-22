69bbe3ab70674934f79a97a50d56ec5881351ed9
utzcoz <utzcoz@outlook.com>
2020-03-22 21:46:13 +0800

    Run test with module name
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- run-test.sh
+++ run-test.sh
@@ -3,6 +3,6 @@
 # for more information
 # Before run shell script, you should ensure there is a connected device
 # Run intrumentation unit test
-./gradlew connectedAndroidTest
+./gradlew :test-app:connectedAndroidTest
 # Show the test summary result
 python3 junit_result_parser.py -d test-app/build/outputs/androidTest-results/connected/
